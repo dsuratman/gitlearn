be126284a8ca1a1e96ed06501c857500b240a24b
dody suratman <dody.suratman@gmail.com>
2020-10-04 12:15:18 -0700

    dev: added i + j

--- gitlearn.c
+++ gitlearn.c
@@ -7,6 +7,11 @@
  */
 int main(void)
 {
+	unsigned int i, j;
+
+	i = 2;
+	j = 3;
+	printf("i + j = %d\n", i+j);
 	/* Let's add more comment here */
 	/* This is a special mod in master only */
 	/* dev branch commit 8 */

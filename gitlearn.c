92ea7c10e0ec8416e5ad18168a0c4333e384ddcf
dody suratman <dody.suratman@gmail.com>
2020-10-04 12:28:04 -0700

    merging changes from dev

--- gitlearn.c
+++ gitlearn.c
@@ -15,8 +15,17 @@ int substract(unsigned int a, unsigned int b)
  */
 int main(void)
 {
+	unsigned int i, j;
+
+	i = 2;
+	j = 3;
+	printf("i + j = %d\n", i+j);
+
+	printf("i * j = %d\n", i*j);
+
 	/* Let's add more comment here */
 	/* This is a special mod in master only */
 	/* dev branch commit 8 */
+	/* dev branch commit 7 */
 	return 0;
 }
